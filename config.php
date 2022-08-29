fb8366fb9f6080a9bcd061b0e79436666fe4633b
Marcelo <109112878+Marcelo64@users.noreply.github.com>
2022-08-29 14:53:49 -0700

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $dbConection=mysqli_connect("localhost","root","","shelfishrd_db");
-    $dbServername = "localhost";
-    $dbUsername = "root";
-    $dbPass = "";
-    $dbName = "shelfishrd_db";
-?>
