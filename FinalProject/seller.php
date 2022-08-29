f581169d5390596e29843065ab73c422dfdfba53
Samridh Chawla <samridhchawla95@gmail.com>
2022-08-28 23:57:54 -0700

    samridh code added

--- FinalProject/seller.php
+++ FinalProject/seller.php
@@ -36,7 +36,7 @@
                         $dbcon = mysqli_connect("localhost","root","","books_db");
                         if($dbcon->connect_error){
                             echo "<h1>".$db~con->connect_error."</h1>";
-                        }else{~
+                        }else{
                             $insertCmd = "INSERT INTO books_tb (productName,authorName,productDetails,price) VALUES ('".$productName."','".$authorName."','".$productDetails."','".$price."')";
                             if($dbcon->query($insertCmd)===TRUE){
                                 echo "<h1>Ad posted</h1>";
