af9e4c67577d924d0c2ffeb70cf717d996c0b81a
alycehwy <pary2451@gmail.com>
2022-09-03 01:50:00 -0700

    update addbook page

--- panelAdmin/pages/curbrw.php
+++ panelAdmin/pages/curbrw.php
@@ -20,21 +20,19 @@
                     die("Connection error");
                 }
                 else{
-                    $bookSelect = "SELECT * FROM borrow_tb INNER JOIN books_tb ON borrow_tb.b_id = books_tb.b_id INNER JOIN user_tb ON borrow_tb.user_id = user_tb.user_id";
+                    $bookSelect = "SELECT * FROM borrow_tb INNER JOIN books_tb ON borrow_tb.b_id = books_tb.b_id INNER JOIN user_tb ON borrow_tb.buser_id = user_tb.user_id WHERE borrow_tb.status ='borrowing'";
                     $result = $dbConection->query($bookSelect);
                     while($row = $result->fetch_assoc()){
-                        if($row['status'] == 'borrowing'){
-                            echo "<tr class='border-secondary'>";
-                            echo "<td>".$row['borrow_id']."</td>";
-                            echo "<td>".$row['user_id']."</td>";
-                            echo "<td>".$row['username']."</td>";
-                            echo "<td>".$row['b_id']."</td>";
-                            echo "<td>".$row['b_title']."</td>";
-                            echo "<td>".$row['b_author']."</td>";
-                            echo "<td>".$row['issue_date']."</td>";
-                            echo "<td>".$row['expiry_date']."</td>";
-                            echo "</tr>";
-                        }
+                        echo "<tr class='border-secondary'>";
+                        echo "<td>".$row['borrow_id']."</td>";
+                        echo "<td>".$row['buser_id']."</td>";
+                        echo "<td>".$row['username']."</td>";
+                        echo "<td>".$row['b_id']."</td>";
+                        echo "<td>".$row['b_title']."</td>";
+                        echo "<td>".$row['b_author']."</td>";
+                        echo "<td>".$row['issue_date']."</td>";
+                        echo "<td>".$row['expiry_date']."</td>";
+                        echo "</tr>";
                     }
                     $dbConection->close();
                 }
